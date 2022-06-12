42a8903b7a0da9684741d33fc235545ac1b5d7bc
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-06-12 17:00:23 +0530

    Add files via upload

---
+++ Escape_seq.py
@@ -0,0 +1,64 @@
+# print('bye\akhil') # byekhil
+# print('bye\aakhil') # byeakhil
+# print('bye\\akhil') # bye\akhil
+
+# print('hi\bharath') # hharath
+# print('hi \bharath') # hiharath
+# print('hi\\bharath') # hi\bharath
+
+# print('hi\charan')
+# print('hi\david')
+# print('hi\eshwar')
+
+# print('hi\fasi')
+
+# print('hi\ganesh')
+# print('hi\harish')
+# print('hi\ishan')
+# print('hi\jai')
+# print('hi\karthik')
+# print('hi\lakshman')
+# print('hi\manish')
+
+# print('hi\navya')
+
+# print('hi\omprakash')
+# print('hi\pankaj')
+# print('hi\queen')
+
+# print('hi\ramesh')# amesh
+# print('hi\ram') # am
+
+# print('hello\ram') # amllo
+
+# print('hello\ramesh') # amesh
+# print('hello\rames') # ameso
+# print('heyhi\ramya ') # amya 
+
+# print('hi\system')
+
+# print('hello\\ten')
+
+# print('hi\umesh')
+
+# print('\u0001')
+# print('\u0002')
+# print('\u0003')
+# print('\u0004')
+# print('\u0005')
+# print('\u0006')
+# print('\u0007')
+# print('\u0008')
+# print('\u0009')
+
+# print('hi\vriddi')
+
+# print('hi\worng')
+
+# print('hi\xavier')
+
+# print('hi\yash')
+# print('hi\zayn')
+
+# print('hi\Naveen')
+# print('hi\ Usha')
